54f73be3c2f080b1de9a77f2a96e3b1d9a65dbfe
freshmea <freshmea@naver.com>
2023-09-04 11:47:31 +0900

    special method

--- pytorch/torch2.py
+++ pytorch/torch2.py
@@ -6,18 +6,26 @@ class PyTorch(Torch):
         super().__init__(name)
         self.pyname = 'py최수길'
 
-    def sub_print(self):
-        print('파이네임 : ' + self.pyname+ '\n이름은 : ' + self.name)
+    def __str__(self):
+        return '파이네임 : ' + self.pyname+ '\n이름은 : ' + self.name
+    
+    def __eq__(self, value):
+        return self.pyname == value.pyname
 
 
 def main():
     # t = torch1.Torch('최수길')
     t = Torch('최수길')
     t2 = PyTorch('최아무개')
+    t3 = PyTorch('최아무개')
     t.print()
     # t.subprint() # 부모 클래스에 없음.
     t2.print()
-    t2.sub_print()
+    print(t2)
+    # print(isinstance(t, object))
+    # 비교연산자 __eq__ -> ==
+    print(t2 == t3)
+    # 비교연산자 __gt__ -> < __le__ -> < __ne__ -> 
 
 if __name__ == '__main__':
     main()
