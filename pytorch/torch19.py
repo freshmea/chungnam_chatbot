1bbce7f1d2c6ebab3563015e95be8af47d4f1931
freshmea <freshmea@naver.com>
2023-09-07 15:47:27 +0900

    cat and dog

---
+++ pytorch/torch19.py
@@ -0,0 +1,35 @@
+import os, time, copy, glob, cv2, shutil
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+import torchvision.models as models
+from torch.utils.data import Dataset, DataLoader
+
+data_path = 'pytorch/data/catanddog/train'
+
+transform = transforms.Compose(
+    [
+        transforms.Resize([256,256]),
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor()
+    ]
+)
+
+train_dataset = torchvision.datasets.ImageFolder(
+    data_path, transform=transform,
+)
+
+train_loader = DataLoader(
+    train_dataset, batch_size=32, num_workers=8, shuffle=True
+)
+
+print(len(train_dataset))
