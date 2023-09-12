0242f1f1ac9759393b708ff9c4c2edfea459050c
freshmea <freshmea@naver.com>
2023-09-12 14:47:21 +0900

    display

--- pytorch/torch21.py
+++ pytorch/torch21.py
@@ -295,3 +295,31 @@ res.reset_index(drop=True, inplace=True)
 res.to_csv(r"C:\chungnam_chatbot\pytorch\data", index=False)
 
 res.head()
+
+# 14 번 셀 ---------------------------------
+class_ = Classes = {0: "cat", 1: "dog"}
+
+
+def display_image_grid2(images_filepaths, predicted_labels=(), cols=5):
+    rows = len(images_filepaths) // cols
+    figure, ax = plt.subplots(nrows=rows, ncols=cols, figsize=(12, 6))
+    for i, image_filepath in enumerate(images_filepaths):
+        image = cv2.imread(image_filepath)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        # true_label = os.path.normpath(image_filepath).split(os.sep)[-2]
+        # predicted_label = predicted_labels[i] if predicted_labels else true_label
+        # color = "green" if true_label == predicted_label else "red"
+        a = random.choice(res["id"].values)
+        label = res.loc[res["id"] == a, "label"].values[0]
+        if label > 0.5:
+            label = 1
+        else:
+            label = 0
+        ax.ravel()[i].imshow(image)
+        ax.ravel()[i].set_title(class_[label])
+        ax.ravel()[i].set_axis_off()
+    plt.tight_layout()
+    plt.show()
+
+
+display_image_grid2(test_image_filepaths)
