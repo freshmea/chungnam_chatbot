dc87a5e7e59e1893404e4141cf252059d9cba8af
freshmea <freshmea@naver.com>
2023-09-13 12:46:27 +0900

    contact

--- flaskbook/apps/minimalapp/app.py
+++ flaskbook/apps/minimalapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, current_app, g, request, url_for, redirect
 
 app = Flask(__name__)
 
@@ -16,3 +16,18 @@ def hello(name):
 @app.route("/name/<name>")
 def show_name(name):
     return render_template("index.html", name=name)
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/contact/complete", methods=["GET", "POST"])
+def contact_complete():
+    if request.method == "POST":
+        # 이메일 보내기
+        # contact 로 리다이렉트 하기
+        return redirect(url_for("contact_complete"))
+
+    return render_template("contact_complete.html")
