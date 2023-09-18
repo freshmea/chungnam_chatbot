8b170735d886ef9c3201384f3910f7bba9957a31
freshmea <freshmea@naver.com>
2023-09-19 04:04:59 +0900

    copilot

--- python/1_hello.py
+++ python/1_hello.py
@@ -1,1 +1,13 @@
-print('Hello, python!')
+# 연속으로 hello, python을 출력하는 코드
+# input으로 받은 숫자만큼 반복하여 출력한다.
+# 2023.09.19
+# author: choi sugil
+# license: MIT
+def main():
+    input_num = int(input("반복할 횟수를 입력하세요: "))
+    for _ in range(input_num):
+        print("Hello, python!")
+
+
+if __name__ == "__main__":
+    main()
--- python/2_keyward.py
+++ python/2_keyward.py
@@ -1,4 +1,10 @@
+# author: choi sugil
+# brief: keyward
+# date: 2019.12.02
+# version: 1.0.0
+# license: MIT
 import keyword
+
 print(keyword.kwlist)
 for i in keyword.kwlist:
     print(i)
