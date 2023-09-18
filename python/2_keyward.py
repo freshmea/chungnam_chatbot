9c958966393d86bde3fea6861cb973644d5e4fca
freshmea <freshmea@naver.com>
2023-09-19 04:32:28 +0900

    copilot

--- python/2_keyward.py
+++ python/2_keyward.py
@@ -1,11 +1,15 @@
 # author: choi sugil
-# brief: keyward
-# date: 2019.12.02
-# version: 1.0.0
-# license: MIT
+# date: 2019.12.02 version: 1.0.0 license: MIT brief: keyward
+
 import keyword
 
-print(keyword.kwlist)
-for i in keyword.kwlist:
-    print(i)
-print(len(keyword.kwlist))
+
+def main():
+    print(keyword.kwlist)
+    for i in keyword.kwlist:
+        print(i)
+    print(len(keyword.kwlist))
+
+
+if __name__ == "__main__":
+    main()
