c9088858c8283974c7b2b270203ada98a0102b83
freshmea <freshmea@naver.com>
2023-09-18 12:25:57 +0900

    logout

--- flaskbook/apps/auth/views.py
+++ flaskbook/apps/auth/views.py
@@ -2,7 +2,7 @@ from apps.app import db
 from apps.auth.forms import SignUpForm, LoginForm
 from apps.crud.models import User
 from flask import Blueprint, render_template, flash, url_for, redirect, request
-from flask_login import login_user
+from flask_login import login_user, logout_user
 
 auth = Blueprint("auth", __name__, template_folder="templates", static_folder="static")
 
@@ -44,3 +44,9 @@ def login():
             return redirect(url_for("crud.users"))
         flash("메일 주소 또는 비밀번호가 일치하지 않습니다.")
     return render_template("auth/login.html", form=form)
+
+
+@auth.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("auth.login"))
