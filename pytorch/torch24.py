bb7a7a355b3b7bb41eff06533f2fb6a8556d8aeb
freshmea <freshmea@naver.com>
2023-09-20 11:59:05 +0900

    splite pretrain-text

--- pytorch/torch24.py
+++ pytorch/torch24.py
@@ -5,6 +5,8 @@ import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 import time
+import string
+import random
 
 # 2번 셀
 start = time.time()
@@ -18,3 +20,14 @@ train_data, test_data = torchtext.datasets.IMDB.splits(TEXT, LABEL)
 print(vars(train_data.examples[0]))
 
 # 5번 셀
+
+for example in train_data.examples:
+    text = [x.lower() for x in vars(example)["text"]]
+    text = [x.replace("<br", "") for x in text]
+    text = ["".join(c for c in s if c not in string.punctuation) for s in text]
+    text = [s for s in text if s]
+    vars(example)["text"] = text
+
+# 6번 셀
+
+train_data, valid_data = train_data.split(random_state=random.seed(0), split_ratio=0.8)
