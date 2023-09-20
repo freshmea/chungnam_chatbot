2fd5c07db6a28acec5e482be234a7c40c1710742
freshmea <freshmea@naver.com>
2023-09-20 12:49:14 +0900

    cnn train def

--- pytorch/torch24.py
+++ pytorch/torch24.py
@@ -31,3 +31,117 @@ for example in train_data.examples:
 # 6번 셀
 
 train_data, valid_data = train_data.split(random_state=random.seed(0), split_ratio=0.8)
+
+# 7번 셀
+print("Number of training example: ", len(train_data))
+print("Number of validating example: ", len(valid_data))
+print("Number of testing examples: ", len(test_data))
+
+# 8번 셀
+TEXT.build_vocab(train_data, max_size=10000, min_freq=10, vectors=None)
+LABEL.build_vocab(train_data)
+
+print("TEXT tokens Vocabulary size: ", len(TEXT.vocab))
+print("Label tokens size: ", len(LABEL.vocab))
+print(LABEL.vocab.stoi)
+
+# 9번 셀
+BATCH_SIZE = [64 for _ in range(100)]
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+embeding_dim = 100
+hidden_size = 300
+
+train_iter, valid_iter, test_iter = torchtext.data.BucketIterator.splits(
+    (train_data, valid_data, test_data), batch_sizes=BATCH_SIZE, device=device
+)
+
+
+# 10번 셀
+class RNNCell_Encoder(nn.Module):
+    def __init__(self, input_dim, hidden_size):
+        super().__init__()
+        self.rnn = nn.RNNCell(input_dim, hidden_size)
+
+    def forward(self, inputs):
+        bz = inputs.shape[1]
+        ht = torch.zeros(bz, hidden_size, device=device)
+        for word in inputs:
+            ht = self.rnn(word, ht)
+        return ht
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.em = nn.Embedding(len(TEXT.vocab.stoi), embeding_dim)
+        self.rnn = RNNCell_Encoder(embeding_dim, hidden_size)
+        self.fc1 = nn.Linear(hidden_size, 256)
+        self.fc2 = nn.Linear(256, 3)
+
+    def forward(self, x):
+        x = self.em(x)
+        x = self.rnn(x)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+
+# 11번 셀
+model = Net()
+model.to(device)
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+
+# 12번 셀
+def training(epoch, model, trainloader, validloader):
+    correct = 0
+    total = 0
+    running_loss = 0
+
+    model.train()
+    for b in trainloader:
+        x, y = b.text, b.label
+        x, y = x.to(device), y.to(device)
+        y_pred = model(x)
+        loss = loss_fn(y_pred, y)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        with torch.no_grad():
+            predicted = torch.argmax(y_pred, dim=1)
+            correct += (predicted == y).sum().item()
+            total += y.size(0)
+            running_loss += loss.item()
+
+    epoch_loss = running_loss / len(trainloader)
+    epoch_acc = correct / total
+
+    valid_correct = 0
+    valid_total = 0
+    valid_running_loss = 0
+
+    model.eval()
+    with torch.no_grad():
+        for b in validloader:
+            x, y = b.text, b.label
+            x, y = x.to(device), y.to(device)
+            y_pred = model(x)
+            loss = loss_fn(y_pred, y)
+            predicted = torch.argmax(y_pred, dim=1)
+            valid_correct += (predicted == y).sum().item()
+            valid_total += y.size(0)
+            valid_running_loss += loss.item()
+
+    epoch_valid_loss = valid_running_loss / len(validloader)
+    epoch_valid_acc = valid_correct / valid_total
+
+    print(
+        f"epoch: {epoch}",
+        f"train loss: {epoch_loss:.4f}",
+        f"train acc: {epoch_acc:.4f}",
+        f"valid loss: {epoch_valid_loss:.4f}",
+        f"valid acc: {epoch_valid_acc:.4f}",
+    )
+    return epoch_loss, epoch_acc, epoch_valid_loss, epoch_valid_acc
