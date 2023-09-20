95180db9ceffc348a29d5217b824d62c56d45440
freshmea <freshmea@naver.com>
2023-09-20 10:48:45 +0900

    cnnpredict

--- pytorch/torch23.py
+++ pytorch/torch23.py
@@ -2,6 +2,8 @@ import pandas as pd
 from datetime import datetime
 from statsmodels.tsa.arima.model import ARIMA
 import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.metrics import mean_squared_error
 
 
 def parser(x):
@@ -23,3 +25,25 @@ residuals.plot()
 residuals.plot(kind="kde")
 plt.show()
 print(residuals.describe())
+
+
+X = series.values
+X = np.nan_to_num(X)
+size = int(len(X) * 0.66)
+train, test = X[0:size], X[size : len(X)]
+history = [x for x in train]
+predictinos = list()
+for t in range(len(test)):
+    model = ARIMA(history, order=(5, 1, 0))
+    model_fit = model.fit()
+    output = model_fit.forecast()
+    yhat = output[0]
+    predictinos.append(yhat)
+    obs = test[t]
+    history.append(obs)
+    print(f"predicted={yhat}, expected={obs}")
+error = mean_squared_error(test, predictinos)
+print(f"Test MSE: {error}")
+plt.plot(test)
+plt.plot(predictinos, color="red")
+plt.show()
