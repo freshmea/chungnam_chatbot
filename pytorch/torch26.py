305f73b95cbb9b0757e2f41130f22d9cbf02525d
freshmea <freshmea@naver.com>
2023-09-21 14:45:29 +0900

    aa

--- pytorch/torch26.py
+++ pytorch/torch26.py
@@ -54,4 +54,63 @@ num_epochs = n_iters / (len(train_dataset) / batch_size)
 num_epochs = int(num_epochs)
 print(len(train_dataset), batch_size, num_epochs)
 
+
 # 6
+class LSTMcell(nn.Module):
+    def __init__(self, input_size, hidden_size, bias=True):
+        super().__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.bias = bias
+        self.x2h = nn.Linear(input_size, 4 * hidden_size, bias=bias)
+        self.h2h = nn.Linear(hidden_size, 4 * hidden_size, bias=bias)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        std = 1.0 / math.sqrt(self.hidden_size)
+        for w in self.parameters():
+            w.data.uniform_(-std, std)
+
+    def forward(self, x, hidden):
+        hx, cx = hidden
+        x = x.view(-1, x.size(1))
+
+        gates = self.x2h(x) + self.h2h(hx)
+        gates = gates.squeeze()
+        ingate, forgetgate, cellgate, outgate = gates.chunk(4, 1)
+
+        ingate = F.sigmoid(ingate)
+        forgetgate = F.sigmoid(forgetgate)
+        cellgate = F.tanh(cellgate)
+        outgate = F.sigmoid(outgate)
+
+        cy = torch.mul(cx, forgetgate) + torch.mul(ingate, cellgate)
+        hy = torch.mul(outgate, F.tanh(cy))
+        return (hy, cy)
+
+
+# 7
+class LSTMModel(nn.Module):
+    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim, bias=True):
+        super().__init__()
+        self.hidden_dim = hidden_dim
+        self.layer_dim = layer_dim
+        self.lstm = LSTMcell(input_dim, hidden_dim, layer_dim)
+        self.fc = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        h0 = Variable(
+            torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).to(device)
+        )
+        c0 = Variable(
+            torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).to(device)
+        )
+        outs = []
+        cn = c0[0, :, :]
+        hn = h0[0, :, :]
+        for seq in range(x.size(1)):
+            hn, cn = self.lstm(x[:, seq, :], (hn, cn))
+            outs.append(hn)
+        out = outs[-1].squeeze()
+        out = self.fc(out)
+        return out
