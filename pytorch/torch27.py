e04492a3b0f49f3477ed8cd62faac5c56b552e96
freshmea <freshmea@naver.com>
2023-09-22 12:08:04 +0900

    lstm to device cuda:0

---
+++ pytorch/torch27.py
@@ -0,0 +1,140 @@
+# 1
+import os
+import time
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+from torch.utils.data import TensorDataset, DataLoader
+from torch.autograd import Variable
+from tqdm import tqdm_notebook
+
+# from tqdm.notebook import tqdm
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.model_selection import train_test_split
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# 2
+data = pd.read_csv("data/SBUX.csv")
+print(data.dtypes)
+
+# 3
+data["Date"] = pd.to_datetime(data["Date"])
+# data = data.set_index("Date")
+data.set_index("Date", inplace=True)
+# data['volume'] = data["volume"].astype(float)
+data["Volume"].astype(float, inplace=True)
+print(data.dtypes)
+
+# 4
+X = data.iloc[:, :-1]
+y = data.iloc[:, 5:6]
+print(X)
+print(y)
+
+# 5
+ms = MinMaxScaler()
+ss = StandardScaler()
+
+X_ss = ss.fit_transform(X)
+y_ms = ms.fit_transform(y)
+
+X_train = X_ss[:200, :]
+X_test = X_ss[200:, :]
+
+y_train = y_ms[:200, :]
+y_test = y_ms[200:, :]
+
+print("Training Shape: ", X_train.shape, X_test.shape)
+print("Testing Shape: ", y_train.shape, y_test.shape)
+
+# 6
+X_train_tensors = Variable(torch.Tensor(X_train))
+X_test_tensors = Variable(torch.Tensor(X_test))
+
+y_train_tensors = Variable(torch.Tensor(y_train))
+y_test_tensors = Variable(torch.Tensor(y_test))
+print("Training Shape", X_train_tensors.shape, y_train_tensors.shape)
+print("Testing Shape", X_test_tensors.shape, y_test_tensors.shape)
+X_train_tensors_f = torch.reshape(
+    X_train_tensors, (X_train_tensors.shape[0], 1, X_train_tensors.shape[1])
+)
+X_test_tensors_f = torch.reshape(
+    X_test_tensors, (X_test_tensors.shape[0], 1, X_test_tensors.shape[1])
+)
+
+print("Training Shape", X_train_tensors_f.shape, y_train_tensors.shape)
+print("Testing Shape", X_test_tensors_f.shape, y_test_tensors.shape)
+
+
+# 7
+# class LSTMcell(nn.Module):
+# ------
+# nn.LSTM
+
+
+class LSTMModule(nn.Module):
+    def __init__(self, num_classes, input_size, hidden_size, num_layers, seq_length):
+        super().__init__()
+        self.num_classes = num_classes
+        self.num_layers = num_layers
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.seq_length = seq_length
+
+        self.lstm = nn.LSTM(
+            input_size=input_size,
+            hidden_size=hidden_size,
+            num_layers=num_layers,
+            batch_first=True,
+        )
+        self.fc_1 = nn.Linear(hidden_size, 128)
+        self.fc = nn.Linear(128, num_classes)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        h_0 = Variable(
+            torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
+        )
+        c_0 = Variable(
+            torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
+        )
+        ouput, (hn, cn) = self.lstm(x, (h_0, c_0))
+        hn = hn.view(-1, self.hidden_size)
+        out = self.relu(hn)
+        out = self.fc_1(out)
+        out = self.relu(out)
+        out = self.fc(out)
+        return out
+
+
+# 8
+num_epochs = 1000
+learning_rate = 0.0001
+
+input_size = 5
+hidden_size = 2
+num_layers = 1
+
+num_classes = 1
+model = LSTMModule(
+    num_classes, input_size, hidden_size, num_layers, X_train_tensors_f.shape[1]
+).to(device)
+
+criterion = torch.nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+
+# 9
+for epoch in range(num_epochs):
+    outputs = model(X_train_tensors_f.to(device))
+    optimizer.zero_grad()
+    loss = criterion(outputs, y_train_tensors.to(device))
+    loss.backward()
+    optimizer.step()
+    if epoch % 100 == 0:
+        print("Epoch: %d, Loss: %1.5f" % (epoch, loss.item()))
