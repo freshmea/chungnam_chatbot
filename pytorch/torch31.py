b8ef45ab686973af2ac4b21ed6490005b9a2dc43
freshmea <freshmea@naver.com>
2023-09-25 12:19:55 +0900

    debug

--- pytorch/torch31.py
+++ pytorch/torch31.py
@@ -36,13 +36,13 @@ val_transform = transforms.Compose(
 
 # 3
 train_dataset = datasets.ImageFolder(
-    root="pytorch/data/archive/train", transform=train_transform
+    root="data/archive/train", transform=train_transform
 )
 val_dataset = datasets.ImageFolder(
-    root="pytorch/data/archive/test", transform=val_transform
+    root="data/archive/test", transform=val_transform
 )
 train_dataloder = DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers=4)
-val_dataset = DataLoader(val_dataset, batch_size=32, shuffle=False, num_workers=4)
+val_dataloder = DataLoader(val_dataset, batch_size=32, shuffle=False, num_workers=4)
 
 
 # 4
@@ -76,7 +76,6 @@ class LRScheduler:
 
     def __call__(self, val_loss):
         self.lr_scheduler.step(val_loss)
-
 
 # 6 early stopping
 class EarlyStopping:
@@ -113,13 +112,13 @@ class EarlyStopping:
 
 
 # 7 parser
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--lr-scheduler", dest="lr_scheduler", action="store_true", default=False
-)
-parser.add_argument(
-    "--early-stopping", dest="early_stopping", action="store_true", default=False
-)
+# parser = argparse.ArgumentParser()
+# parser.add_argument(
+#     "--lr-scheduler", dest="lr_scheduler", action="store_true", default=False
+# )
+# parser.add_argument(
+#     "--early-stopping", dest="early_stopping", action="store_true", default=False
+# )
 # args = vars(parser.parse_args())
 
 
@@ -131,6 +130,34 @@ print(f"{total_params:,} total parameters")
 total_trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
 print(f"{total_trainable_params:,} training parameters\n")
 
+# 8.1 lr scheduler
+lr = 0.001
+epoch_num = 10
+optimizer = optim.Adam(model.parameters(), lr=lr)
+criterion = nn.CrossEntropyLoss()
+
+loss_plot_name = 'loss'
+acc_plot_name = 'accuracy'
+model_name = 'resnet50'
+
+
+lr_scheduler_bool = False
+early_stopping_bool = False
+if lr_scheduler_bool:
+    print('info: init lr scheduler')
+    lr_scheduler = LRScheduler(optimizer)
+    loss_plot_name = 'loss_lr_scheduler'
+    acc_plot_name = 'accuracy_lr_scheduler'
+    model_name = 'resnet50_lr_scheduler'
+if early_stopping_bool:
+    print('info: init early stopping')
+    early_stopping = EarlyStopping()
+    loss_plot_name = 'loss_early_stopping'
+    acc_plot_name = 'accuracy_early_stopping'
+    model_name = 'resnet50_early_stopping'
+
+
+
 
 # 9 training
 def training(model, train_dataloader, train_dataset, optimizer, criterion):
@@ -139,7 +166,7 @@ def training(model, train_dataloader, train_dataset, optimizer, criterion):
     train_running_correct = 0
     counter = 0
     total = 0
-    prog_bar = tqdm_notebook(train_dataloader, desc="Training", leave=True)
+    prog_bar = tqdm_notebook(enumerate(train_dataloader), total=int(len(train_dataset)/train_dataloader.batch_size))
 
     for i, data in prog_bar:
         counter += 1
@@ -157,3 +184,61 @@ def training(model, train_dataloader, train_dataset, optimizer, criterion):
     train_loss = train_running_loss / counter
     train_accuracy = 100.0 * train_running_correct / total
     return train_loss, train_accuracy
+
+
+
+# 10 validate
+def validate(model, test_dataloader, val_dataset, criterion):
+    print('VALIDATION')
+    model.eval()
+    val_running_loss = 0.0
+    val_running_correct = 0
+    counter = 0
+    total = 0
+    prog_bar = tqdm_notebook(enumerate(test_dataloader), total=int(len(val_dataset)/test_dataloader.batch_size))
+
+    with torch.no_grad():
+        for i, data in prog_bar:
+            counter += 1
+            data, target = data[0].to(device), data[1].to(device)
+            total += target.size(0)
+            outputs = model(data)
+            loss = criterion(outputs, target)
+
+            val_running_loss += loss.item()
+            _, preds = torch.max(outputs.data, 1)
+            val_running_correct += (preds == target).sum().item()
+
+        val_loss = val_running_loss / counter
+        val_accuracy = 100.0 * val_running_correct / total
+        return val_loss, val_accuracy
+
+
+# 11 model training
+train_loss, train_accuracy = [], []
+val_loss, val_accuracy = [], []
+
+
+start = time.time()
+for epoch in range(epoch_num):
+    print(f"Epoch {epoch+1} of {epoch_num}")
+    train_epoch_loss, train_epoch_accuracy = training(
+        model, train_dataloder, train_dataset, optimizer, criterion)
+    val_epoch_loss, val_epoch_accuracy = validate(
+        model, val_dataloder, val_dataset, criterion)
+    train_loss.append(train_epoch_loss)
+    train_accuracy.append(train_epoch_accuracy)
+    val_loss.append(val_epoch_loss)
+    val_accuracy.append(val_epoch_accuracy)
+    if lr_scheduler_bool:
+        lr_scheduler(val_epoch_loss)
+    if early_stopping_bool:
+        early_stopping(val_epoch_loss, model)
+        if early_stopping.early_stop:
+            print("Early stopping")
+            break
+
+    print(f"Train Loss: {train_epoch_loss:.4f}, Train Acc: {train_epoch_accuracy:.2f}")
+    print(f'Val Loss: {val_epoch_loss:.4f}, Val Acc: {val_epoch_accuracy:.2f}')
+    print(f'Time: {time.time()-start:.2f} sec')
+    print('-'*50)
