98ecb61d49a7d16022f6d46c0de174de9f012778
freshmea <freshmea@naver.com>
2023-09-27 11:23:49 +0900

    bert 7

--- pytorch/torch39.py
+++ pytorch/torch39.py
@@ -50,7 +50,49 @@ valid_loader = DataLoader(valid_dataset, batch_size=2, shuffle=True, num_workers
 test_dataset = BertDataset(test_df)
 test_loader = DataLoader(test_dataset, batch_size=2, shuffle=True, num_workers=0)
 
-# tokenizer
+# 6 tokenizer
 tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
 model = BertForSequenceClassification.from_pretrained("bert-base-uncased")
 model.to(device)
+
+
+# 7 optimized model save
+def save_checkpoint(save_path, model, valid_loss):
+    if not save_path:
+        return
+    state_dict = {"model_state_dict": model.state_dict(), "valid_loss": valid_loss}
+    torch.save(state_dict, save_path)
+    print(f"Model saved to ==> {save_path}")
+
+
+def load_checkpoint(load_path, model):
+    if not load_path:
+        return
+    state_dict = torch.load(load_path, map_location=device)
+    print(f"Model loaded from <== {load_path}")
+    model.load_state_dict(state_dict["model_state_dict"])
+    return state_dict["valid_loss"]
+
+
+def save_metrics(save_path, train_loss_list, valid_loss_list, global_steps_list):
+    if not save_path:
+        return
+    state_dict = {
+        "train_loss_list": train_loss_list,
+        "valid_loss_list": valid_loss_list,
+        "global_steps_list": global_steps_list,
+    }
+    torch.save(state_dict, save_path)
+    print(f"Model saved to ==> {save_path}")
+
+
+def load_metrics(load_path):
+    if not load_path:
+        return
+    state_dict = torch.load(load_path, map_location=device)
+    print(f"Model loaded from <== {load_path}")
+    return (
+        state_dict["train_loss_list"],
+        state_dict["valid_loss_list"],
+        state_dict["global_steps_list"],
+    )
