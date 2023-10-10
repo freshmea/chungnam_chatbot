410fecde072435859e1907fd6d1276e0ffe444d5
freshmea <freshmea@naver.com>
2023-10-10 11:49:54 +0900

    autoencoder-digit

--- pytorch/torch46.py
+++ pytorch/torch46.py
@@ -1,5 +1,6 @@
 # auto-encoder example 0-9 digit
 # 1 import libraries
+from pathlib import PosixPath
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -96,7 +97,7 @@ params_to_optimize = [
     {"params": encode.parameters()},
     {"params": decoder.parameters()},
 ]
-optim = torch.optim.Adam(params_to_optimize, lr=0.001, weight_decay=1e-05)
+optim_ob = torch.optim.Adam(params_to_optimize, lr=0.001, weight_decay=1e-05)
 loss_fn = torch.nn.MSELoss()
 
 
@@ -126,3 +127,95 @@ def train_epoch(
         optimizer.step()
         train_loss.append(loss.detach().cpu().numpy())
     return np.mean(train_loss)
+
+
+# 7 test function
+def test_epoch(encoder, decoder, device, dataloader, loss_fn, noise_factor=0.3):
+    encoder.eval()
+    decoder.eval()
+
+    with torch.no_grad():
+        conc_out = []
+        conc_label = []
+        for image_batch, _ in dataloader:
+            image_noisy = add_noise(image_batch, noise_factor)
+            image_noisy = image_noisy.to(device)
+            encoded_data = encoder(image_noisy)
+            decoded_data = decoder(encoded_data)
+            conc_out.append(decoded_data.cpu())
+            conc_label.append(image_batch.cpu())
+        conc_out = torch.cat(conc_out)
+        conc_label = torch.cat(conc_label)
+        val_loss = loss_fn(conc_out, conc_label)
+    return val_loss.data
+
+
+# 8 Korean font
+from matplotlib import font_manager
+import pathlib
+
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+font_fname = pathlib.PosixPath("C:/Windows/Fonts/malgun.ttf")
+font_family = font_manager.FontProperties(fname=font_fname).get_name()
+plt.rcParams["font.family"] = font_family
+
+
+# 9 image visualization
+def plot_ae_outputs(encoder, decoder, n=5, noise_factor=0.3):
+    plt.figure(figsize=(10, 4.5))
+    for i in range(n):
+        ax = plt.subplot(3, n, i + 1)
+        img = test_dataset[i][0].unsqueeze(0)
+        image_noisy = add_noise(img, noise_factor)
+        image_noisy = image_noisy.to(device)
+
+        encoder.eval()
+        decoder.eval()
+        with torch.no_grad():
+            rec_img = decoder(encoder(image_noisy))
+
+        plt.imshow(img.cpu().squeeze().numpy(), cmap="gray")
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+        if i == n // 2:
+            ax.set_title("original image")
+        ax = plt.subplot(3, n, i + 1 + n)
+        plt.imshow(image_noisy.cpu().squeeze().numpy(), cmap="gray")
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+        if i == n // 2:
+            ax.set_title("noisy image")
+        ax = plt.subplot(3, n, i + 1 + n + n)
+        plt.imshow(rec_img.cpu().squeeze().numpy(), cmap="gray")
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+        if i == n // 2:
+            ax.set_title("reconstructed image")
+    plt.subplots_adjust(
+        left=0.1, bottom=0.1, right=0.7, top=0.9, wspace=0.3, hspace=0.3
+    )
+    plt.show()
+
+
+# 10 model train
+import numpy as np
+
+num_epochs = 30
+history_da = {"train_loss": [], "test_loss": []}
+loss_fn = torch.nn.MSELoss()
+
+for epoch in range(num_epochs):
+    print(f"Epoch {epoch+1}/{num_epochs}")
+    train_loss = train_epoch(
+        encode, decoder, device, train_loder, loss_fn, optim_ob, noise_factor=0.3
+    )
+    val_loss = test_epoch(
+        encode, decoder, device, test_loader, loss_fn, noise_factor=0.3
+    )
+    history_da["train_loss"].append(train_loss)
+    history_da["test_loss"].append(val_loss)
+    print(
+        f"\n Epoch {epoch+1}/{num_epochs}, Train Loss: {train_loss:.3f}, Test Loss: {val_loss:.3f}"
+    )
+    plot_ae_outputs(encode, decoder, noise_factor=0.3)
