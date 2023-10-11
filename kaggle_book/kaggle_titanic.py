d367d09a997b2bafa2d4bd93926dfcc2bf950b60
freshmea <freshmea@naver.com>
2023-10-11 12:47:00 +0900

    kaggle

---
+++ kaggle_book/kaggle_titanic.py
@@ -0,0 +1,8 @@
+# 1
+import numpy as np
+import pandas as pd
+
+# 2
+train_df = pd.read_csv("./data/train.csv")
+test_df = pd.read_csv("./data/test.csv")
+submission = pd.read_csv("./data/gender_submission.csv")
